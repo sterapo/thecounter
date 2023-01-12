0bc23091aa4732bdb186feb395006001b9c8a6ca
StefaniaG <Stegambino91@gmail.com>
2023-01-12 11:26:21 +0100

    Versione1

---
+++ assets/js/javascript.js
@@ -0,0 +1,53 @@
+
+function counterCreation(tag, id, text) {
+  element = document.createElement(tag);
+  element.id = id;
+  element.innerHTML = text;
+  return element;
+}
+document.body.innerHTML = "<h1>The counter:</h1>";
+
+const container = counterCreation('div', 'container', '');
+document.body.append(container);
+
+const counterValue = counterCreation('div', 'counter', '0');
+container.append(counterValue);
+
+const buttonContainer = counterCreation('div', 'button-container', '');
+container.append(buttonContainer);
+
+const decreaseButton = counterCreation('button', 'decrease', '-');
+buttonContainer.append(decreaseButton);
+
+const resetButton = counterCreation('button', 'reset', 'Reset');
+buttonContainer.append(resetButton);
+
+const increaseButton = counterCreation('button', 'increase', '+');
+buttonContainer.append(increaseButton);
+
+let count = 0;
+
+buttonContainer.addEventListener('click', (event) => {
+  const target = event.target;
+
+  switch (target.id) {
+    case 'decrease':
+      if (count <= 0) {
+        count--;
+      } else {
+        count--;
+      }
+      break;
+    case 'increase':
+      if (count >= -1) {
+        count++;
+      } else {
+        count++;
+      }
+      break;
+    case 'reset':
+        count = 0;
+        break;
+  }
+  counterValue.innerHTML = count;
+});
